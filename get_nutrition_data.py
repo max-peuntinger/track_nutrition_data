61d9369db9cd824703ff9bede1eff1d3348a781c
mxpnt <maxpeuntinger@gmail.com>
2023-08-04 01:03:16 +0200

    fixed bug in writer writing  to same line repeatedly

--- get_nutrition_data.py
+++ get_nutrition_data.py
@@ -16,7 +16,6 @@ API_KEY = os.getenv('API_KEY')
 # CalorieNinjas API base URL
 BASE_URL = 'https://api.calorieninjas.com/v1/nutrition'
 
-
 class InvalidArgumentNumberException(Exception):
     pass
 
@@ -26,21 +25,17 @@ def get_nutrition_data(food_item, weight):
     headers = {
         'X-Api-Key': API_KEY,
     }
-
     params = {
         'query': f"{food_item} {weight}",
     }
-
     try:
         response = requests.get(BASE_URL, headers=headers, params=params)
-
         if response.status_code == 200:
             data = response.json()
             return data
         else:
             print(f"Error: {response.status_code} - {response.text}")
             return None
-
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return None
@@ -51,13 +46,10 @@ def parse_arguments():
     parser.add_argument('--test', action='store_true', help='Run in test mode. Does not write to CSV.')
 
     args = parser.parse_args()
-
     if len(args.items) % 2 != 0:
         sys.stderr.write("Error, please provide a pair of food and weight")
         raise InvalidArgumentNumberException
-
     pairs = [(args.items[i], args.items[i+1]) for i in range(0, len(args.items), 2)]
-
     return pairs, args.test
 
 
@@ -70,12 +62,9 @@ def process_nutrition_data(food_item, weight, nutrition_data):
     for key, value in nutrition_data["items"][0].items():
         if key == "name":
             data[key] = value
-            continue
-        
+            continue  
         data[key] = float(value) * float(weight[:-1])/100.0
-
     print(f"{data}")
-
     return data
 
 
@@ -86,23 +75,23 @@ def write_to_csv(data, f, writer):
         writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writerow(data)
     print("added")
-
     return writer
 
 
 def main():
     pairs, test = parse_arguments()
 
-    with open('nutrition.csv', 'a') as f:
+    with open('nutrition.csv', 'a', newline='') as f:
         writer = None
-
         for food_item, weight in pairs:
             nutrition_data = get_nutrition_data(food_item, weight)
             if nutrition_data:
                 data = process_nutrition_data(food_item, weight, nutrition_data)
                 if not test:
-                        write_to_csv(data, f, writer)
-
+                    if writer is None:
+                        writer = write_to_csv(data, f, writer)
+                    else:
+                        writer.writerow(data)
 
 if __name__ == '__main__':
     main()
